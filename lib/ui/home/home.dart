a0041fdf833d1baecc3fb135d6707ce60f901533
JoeWangu <joewangu2021@outlook.com>
2025-12-05 22:04:10 +0300

    remove unused code

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:inn/ui/utils/item_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,146 +27,179 @@ class _HomePageState extends State<HomePage> {
       ),
 
       body: SafeArea(
-        child: Column(
-          children: [
-            // TOP HEADER
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
-              child: Row(
-                children: [
-                  // Avatar + Greeting
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 24,
-                        backgroundImage: AssetImage(
-                          'assets/img_assets/avatar.png',
-                        ),
-                      ),
-                      const SizedBox(width: 12),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
+        child: ColoredBox(
+          color: cs.onSurface.withValues(alpha: 0.05),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // TOP SECTION
+                ClipRRect(
+                  borderRadius: BorderRadiusGeometry.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                  child: ColoredBox(
+                    color: cs.onPrimary,
+                    child: Column(
+                      children: [
+                        // TOP HEADER
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                          child: Row(
                             children: [
-                              Icon(
-                                Icons.wb_sunny,
-                                color: Colors.amber,
-                                size: 18,
+                              Row(
+                                children: [
+                                  // Stack(
+                                  //   children: [
+                                  //     Positioned.fill(
+                                  //   child: Container(
+                                  //     width: 50.0,
+                                  //     height: 50.0,
+                                  //     decoration: BoxDecoration(
+                                  //       color: cs.tertiaryContainer,
+                                  //     ),
+                                  //     child: ColoredBox(
+                                  //       color: cs.onPrimary,
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                  CircleAvatar(
+                                    radius: 24,
+                                    backgroundColor: cs.onSurface.withValues(
+                                      alpha: 0.05,
+                                    ),
+                                    backgroundImage: AssetImage(
+                                      'assets/img_assets/avatar.png',
+                                    ),
+                                  ),
+                                  //   ],
+                                  // ),
+                                  const SizedBox(width: 12),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.wb_sunny,
+                                            color: Colors.amber,
+                                            size: 18,
+                                          ),
+                                          const SizedBox(width: 6),
+                                          Text(
+                                            'SUNNY',
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              color: cs.surfaceContainerHighest,
+                                              fontWeight: FontWeight.w400,
+                                              decorationThickness: 2.0,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const Text(
+                                        'Jay',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                              const SizedBox(width: 6),
-                              Text(
-                                'SUNNY',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                  color: cs.surfaceContainerHighest,
-                                  fontWeight: FontWeight.w400,
-                                  decorationThickness: 2.0,
+                              const Spacer(),
+                              // Settings Icon
+                              InkWell(
+                                splashFactory: NoSplash.splashFactory,
+                                highlightColor: Colors.transparent,
+                                onTap: () {
+                                  context.pushNamed('login');
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: cs.onSurface.withValues(alpha: 0.05),
+                                    borderRadius: BorderRadius.circular(30),
+                                    // boxShadow: [
+                                    //   BoxShadow(
+                                    //     color: Colors.black12,
+                                    //     blurRadius: 10,
+                                    //     offset: Offset(0, 4),
+                                    //   ),
+                                    // ],
+                                  ),
+                                  child: const Icon(Icons.settings, size: 25),
                                 ),
                               ),
                             ],
                           ),
-                          const Text(
-                            'Jay',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        // SEARCH BAR
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: SizedBox(
+                            height: 45,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Search Destination',
+                                hintStyle: TextStyle(
+                                  color: cs.onSurface.withAlpha(100),
+                                  fontSize: 14,
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  color: cs.onSurface,
+                                ),
+                                filled: true,
+                                fillColor: cs.onSurface.withValues(alpha: 0.05),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                              style: TextStyle(fontSize: 16),
+                              cursorHeight: 20,
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  // Notification Bell
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: cs.onSurface.withAlpha(10),
-                      borderRadius: BorderRadius.circular(30),
-                      // boxShadow: [
-                      //   BoxShadow(
-                      //     color: Colors.black12,
-                      //     blurRadius: 10,
-                      //     offset: Offset(0, 4),
-                      //   ),
-                      // ],
-                    ),
-                    child: const Icon(Icons.notifications_outlined, size: 25),
-                  ),
-                ],
-              ),
-            ),
+                        ),
 
-            const SizedBox(height: 20),
-
-            // SEARCH BAR
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: SizedBox(
-                height: 45,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search Destination',
-                    hintStyle: TextStyle(
-                      color: cs.onSurface.withAlpha(100),
-                      fontSize: 14,
-                    ),
-                    prefixIcon: Icon(Icons.search, color: cs.onSurface),
-                    filled: true,
-                    fillColor: cs.onSurface.withAlpha(10),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                  style: TextStyle(fontSize: 16),
-                  cursorHeight: 20,
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 30),
-
-            // MAIN SCROLLABLE CONTENT
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadiusGeometry.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                child: ColoredBox(
-                  color: cs.onSurface.withAlpha(10),
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Recently Viewed
-                        _sectionHeader('Recently Viewed', () {
-                          context.pushNamed('app_colors');
-                        }),
-                        const SizedBox(height: 16),
-                        _recentlyViewedList(),
-
-                        const SizedBox(height: 32),
-
-                        // Popular Hotels
-                        _sectionHeader('Popular Hotels', () {
-                          context.pushNamed('login');
-                        }),
-                        const SizedBox(height: 16),
-                        _popularHotelsGrid(),
-                        const SizedBox(
-                          height: 100,
-                        ), // Extra space for bottom nav
+                        const SizedBox(height: 30),
                       ],
                     ),
                   ),
                 ),
-              ),
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 15, 20, 10),
+                  child: Column(
+                    children: [
+                      // Recently Viewed
+                      _sectionHeader('Recently Viewed', () {
+                        context.pushNamed('app_colors');
+                      }),
+                      const SizedBox(height: 16),
+                      _recentlyViewedList(),
+
+                      const SizedBox(height: 32),
+
+                      // Popular Hotels
+                      _sectionHeader('Popular Hotels', () {
+                        context.pushNamed('login');
+                      }),
+                      const SizedBox(height: 16),
+                      _popularHotelsGrid(),
+                      const SizedBox(height: 100),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
 
@@ -247,83 +281,13 @@ class _HomePageState extends State<HomePage> {
         scrollDirection: Axis.horizontal,
         itemCount: 5,
         itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.only(right: 12),
-            child: SizedBox(
-              width: 280,
-              height: 250,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                clipBehavior: Clip.antiAlias,
-                elevation: 4,
-                child: Stack(
-                  children: [
-                    // ---------- IMAGE ----------
-                    Positioned.fill(
-                      child: Image.asset(
-                        'assets/images/hotel${index + 1}.jpg',
-                        fit: BoxFit.cover, // fills width *and* height
-                        // optional: add a subtle dark overlay so white text is readable
-                        // color: Colors.black.withAlpha(10),
-                        // colorBlendMode: BlendMode.darken,
-                      ),
-                    ),
-
-                    // ---------- BOTTOM INFO CARD ----------
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      // child: Padding(
-                      // Small padding so the inner “info card” doesn’t touch the outer edges
-                      // padding: const EdgeInsets.all(2),
-                      child: Card(
-                        color: cs.onPrimary.withValues(alpha: 0.8),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        elevation: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 12,
-                            horizontal: 16,
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Aurora Luxe',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                'Oregon, United States',
-                                style: TextStyle(color: Colors.grey[600]),
-                              ),
-                              const SizedBox(height: 12),
-                              Row(
-                                children: [
-                                  _infoChip(Icons.king_bed_outlined, '4 Rooms'),
-                                  const SizedBox(width: 12),
-                                  _infoChip(
-                                    Icons.bathtub_outlined,
-                                    '4 Bathrooms',
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      // ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+          return ItemCard(
+            cs: cs,
+            needsInfoChip: true,
+            index: index,
+            cardWidth: 280.0,
+            cardHeight: 250.0,
+            containerMargin: 12.0,
           );
         },
       ),
@@ -331,6 +295,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _popularHotelsGrid() {
+    ColorScheme cs = Theme.of(context).colorScheme;
+
     return GridView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
@@ -342,17 +308,22 @@ class _HomePageState extends State<HomePage> {
       ),
       itemCount: 6,
       itemBuilder: (context, index) {
+        // return _itemCard(
+        //   cs: cs,
+        //   needsInfoChip: false,
+        //   index: index,
+        // );
         return Container(
           decoration: BoxDecoration(
-            color: Colors.white,
+            color: cs.onPrimary,
             borderRadius: BorderRadius.circular(24),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                blurRadius: 10,
-                offset: Offset(0, 4),
-              ),
-            ],
+            // boxShadow: [
+            //   BoxShadow(
+            //     color: Colors.black12,
+            //     blurRadius: 10,
+            //     offset: Offset(0, 4),
+            //   ),
+            // ],
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -369,7 +340,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.all(12),
+                padding: const EdgeInsets.only(left: 12),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -388,24 +359,6 @@ class _HomePageState extends State<HomePage> {
           ),
         );
       },
-    );
-  }
-
-  Widget _infoChip(IconData icon, String label) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      decoration: BoxDecoration(
-        color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(icon, size: 16, color: Colors.grey[700]),
-          const SizedBox(width: 6),
-          Text(label, style: const TextStyle(fontSize: 12)),
-        ],
-      ),
     );
   }
 }
