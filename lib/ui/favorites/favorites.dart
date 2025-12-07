7068b0a73106a787c3bfb5b66d14be8a9d56dff1
JoeWangu <joewangu2021@outlook.com>
2025-12-08 00:01:47 +0300

    feat: Add FavoritesPage for displaying cart products and total price using Riverpod

---
+++ lib/ui/favorites/favorites.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:inn/providers/cart_provider.dart';
+
+class FavoritesPage extends ConsumerStatefulWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  ConsumerState<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends ConsumerState<FavoritesPage> {
+  bool showCoupon = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final cartProducts = ref.watch(cartProvider);
+    final total = ref.watch(cartTotalProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+        centerTitle: true,
+        // actions: [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Column(
+              children: cartProducts.map((product) {
+                return Container(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    children: [
+                      Image.asset(product.image, width: 60, height: 60),
+                      const SizedBox(width: 10),
+                      Text(product.title),
+                      const Expanded(child: SizedBox()),
+                      Text('\$${product.price}'),
+                    ],
+                  ),
+                );
+              }).toList(), // output cart products here
+            ),
+
+            // output totals here
+            Text('Total price - $total'),
+          ],
+        ),
+      ),
+    );
+  }
+}
