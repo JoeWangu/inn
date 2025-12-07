c2ae067629c4a2e5d513fc11e602593fd3da7504
JoeWangu <joewangu2021@outlook.com>
2025-12-07 23:49:03 +0300

    refactor: Restructure project by moving utilities to shared/ and utils/ and add new screens, models, services, and providers for core features.

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String? username;
+  final String fullName;
+  final String email;
+  final String password;
+
+  User({
+    this.username,
+    required this.fullName,
+    required this.email,
+    required this.password,
+  });
+}
