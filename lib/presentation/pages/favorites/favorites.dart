d2862242e5f8f849b72e09334653de04065b7d8a
JoeWangu <joewangu2021@outlook.com>
2025-12-08 14:43:43 +0300

    refactor: Rename CartPage to FavoritesPage and remove cart-related logic
    The `FavoritesPage` has been refactored by removing the `cartProvider` and `cartTotalProvider` dependencies. All UI elements related to displaying cart products and their total price have been removed from the body of the Scaffold. The `AppBar` title has been changed from 'Your Cart' to 'Favorites'.

--- lib/presentation/pages/favorites/favorites.dart
+++ lib/presentation/pages/favorites/favorites.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:inn/core/providers/cart_provider.dart';
 
 class FavoritesPage extends ConsumerStatefulWidget {
   const FavoritesPage({super.key});
@@ -14,40 +13,12 @@ class _FavoritesPageState extends ConsumerState<FavoritesPage> {
 
   @override
   Widget build(BuildContext context) {
-    final cartProducts = ref.watch(cartProvider);
-    final total = ref.watch(cartTotalProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Cart'),
+        title: const Text('Favorites'),
         centerTitle: true,
         // actions: [],
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          children: [
-            Column(
-              children: cartProducts.map((product) {
-                return Container(
-                  padding: EdgeInsets.only(top: 10, bottom: 10),
-                  child: Row(
-                    children: [
-                      Image.asset(product.image, width: 60, height: 60),
-                      const SizedBox(width: 10),
-                      Text(product.title),
-                      const Expanded(child: SizedBox()),
-                      Text('\$${product.price}'),
-                    ],
-                  ),
-                );
-              }).toList(), // output cart products here
-            ),
-
-            // output totals here
-            Text('Total price - $total'),
-          ],
-        ),
       ),
     );
   }
