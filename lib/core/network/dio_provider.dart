ffa280d09f1638cedd45a149c9eb05f511b8c1d1
JoeWangu <joewangu2021@outlook.com>
2025-12-09 16:30:15 +0300

    feat: Add Dio provider for network requests
    Introduced a new `dio_provider.dart` file that sets up a Dio instance with base URL and timeout configurations. This provider will facilitate network requests throughout the application, ensuring consistent API interaction settings.

---
+++ lib/core/network/dio_provider.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:inn/core/constants.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dio_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+Dio dio(Ref ref) {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: Constants.baseUrl,
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ),
+  );
+  // dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+  return dio;
+}
