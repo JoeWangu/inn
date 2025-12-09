138a8e6cc0401cf3789850b7613867b774582f97
JoeWangu <joewangu2021@outlook.com>
2025-12-09 16:31:47 +0300

    feat: Generate UserModel JSON serialization code
    Added a new `user_model.g.dart` file containing generated code for JSON serialization of the `UserModel` class, which includes fields for `email` and `username`. This enhances the application's data handling capabilities for user information.

---
+++ lib/data/models/user_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_UserModel _$UserModelFromJson(Map<String, dynamic> json) => _UserModel(
+  email: json['email'] as String,
+  username: json['username'] as String,
+);
+
+Map<String, dynamic> _$UserModelToJson(_UserModel instance) =>
+    <String, dynamic>{'email': instance.email, 'username': instance.username};
