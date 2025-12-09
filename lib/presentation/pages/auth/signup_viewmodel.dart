ddae122f66bcd27025dc66373486ef7e5537587a
JoeWangu <joewangu2021@outlook.com>
2025-12-09 16:40:42 +0300

    refactor: Comment out unused code in SignupViewmodel for clarity
    Removed active code in the `signup_viewmodel.dart` file by commenting it out, improving readability and maintainability. This change prepares the file for future enhancements while ensuring that the existing functionality remains intact.

--- lib/presentation/pages/auth/signup_viewmodel.dart
+++ lib/presentation/pages/auth/signup_viewmodel.dart
@@ -1,24 +1,24 @@
-import 'package:inn/data/models/user.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:inn/data/models/user.dart';
+// import 'package:riverpod_annotation/riverpod_annotation.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-part 'signup_viewmodel.g.dart';
+// part 'signup_viewmodel.g.dart';
 
-@riverpod
-class SignupViewmodel extends _$SignupViewmodel {
-  @override
-  void build() async {}
+// @riverpod
+// class SignupViewmodel extends _$SignupViewmodel {
+//   @override
+//   void build() async {}
 
-  Future<bool> signupUser(User? user) async {
-    final SharedPreferencesAsync prefs = SharedPreferencesAsync();
-    if (user != null) {
-      await prefs.setString('userName', user.fullName);
-      await prefs.setString('email', user.email);
-      await prefs.setString('password', user.password);
-      await prefs.setBool('logged_in', true);
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
+//   Future<bool> signupUser(User? user) async {
+//     final SharedPreferencesAsync prefs = SharedPreferencesAsync();
+//     if (user != null) {
+//       await prefs.setString('userName', user.fullName);
+//       await prefs.setString('email', user.email);
+//       await prefs.setString('password', user.password);
+//       await prefs.setBool('logged_in', true);
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
