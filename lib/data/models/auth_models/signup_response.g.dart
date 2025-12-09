eccd8763ef1910556e300e77324604d2e60671ec
JoeWangu <joewangu2021@outlook.com>
2025-12-09 16:39:34 +0300

    feat: Generate SignupResponse JSON serialization code
    Added a new `signup_response.g.dart` file containing generated JSON serialization code for the `SignupResponse` class, which includes fields for `user` and `token`. This enhances the application's data handling capabilities for user sign-up responses.

---
+++ lib/data/models/auth_models/signup_response.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signup_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_SignupResponse _$SignupResponseFromJson(Map<String, dynamic> json) =>
+    _SignupResponse(
+      user: UserModel.fromJson(json['user'] as Map<String, dynamic>),
+      token: json['token'] as String,
+    );
+
+Map<String, dynamic> _$SignupResponseToJson(_SignupResponse instance) =>
+    <String, dynamic>{'user': instance.user, 'token': instance.token};
