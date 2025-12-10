f90530e4866dc57ff52094faa894895ab103551b
JoeWangu <joewangu2021@outlook.com>
2025-12-10 19:33:22 +0300

    feat: Update LoginResponse model to use UserModel and include refresh token
    Modified the `LoginResponse` model to replace the user field with a `UserModel` type and added a new `refresh` token field. This change enhances the structure of the authentication response, aligning it with the updated user data representation.

--- lib/data/models/auth_models/login_response.dart
+++ lib/data/models/auth_models/login_response.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:inn/data/models/user_model.dart';
 
 part 'login_response.freezed.dart';
 part 'login_response.g.dart';
@@ -6,8 +7,9 @@ part 'login_response.g.dart';
 @freezed
 abstract class LoginResponse with _$LoginResponse {
   const factory LoginResponse({
-    required String user,
+    required UserModel user,
     required String token,
+    required String refresh,
     required bool created,
   }) = _LoginResponse;
 
