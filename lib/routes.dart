cee6ce86a20ef08446e48374a46f8de291a60a72
JoeWangu <joewangu2021@outlook.com>
2025-12-10 19:26:45 +0300

    refactor: Update routing logic to improve structure and maintainability
    Refactored the routing setup in `routes.dart` by encapsulating the `GoRouter` initialization within a `createRouter` function. This change enhances the organization of the routing logic and prepares the code for future modifications. Commented out the previous redirect logic for clarity while retaining the route definitions for the application.

--- lib/routes.dart
+++ lib/routes.dart
@@ -7,75 +7,79 @@ import 'package:inn/presentation/pages/explore/explore.dart';
 import 'package:inn/presentation/pages/favorites/favorites.dart';
 import 'package:inn/presentation/pages/home/home.dart';
 import 'package:inn/presentation/pages/onboarding/onboarding.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-final GoRouter router = GoRouter(
-  initialLocation: '/',
-  redirect: (context, state) async {
-    final SharedPreferencesAsync prefs = SharedPreferencesAsync();
-    final bool seenOnboarding = await prefs.getBool('seen_onboarding') ?? false;
-    final bool loggedIn = await prefs.getBool('logged_in') ?? false;
-    final isFirstLaunch = !seenOnboarding;
+// final GoRouter router =
+GoRouter createRouter(String initialPath) {
+  return GoRouter(
+    initialLocation: initialPath,
+/*     // redirect: (context, state) async {
+    //   final SharedPreferencesAsync prefs = SharedPreferencesAsync();
+    //   final bool seenOnboarding =
+    //       await prefs.getBool('seen_onboarding') ?? false;
+    //   final bool loggedIn = await prefs.getBool('logged_in') ?? false;
+    //   final isFirstLaunch = !seenOnboarding;
 
-    // final token = await prefs.getString('auth_token');
-    // final loggedIn = token != null && token.isNotEmpty;
-    // bool loggingIn = state.uri.path == '/login';
-    // current path
-    final path = state.uri.path;
+    //   // final token = await prefs.getString('auth_token');
+    //   // final loggedIn = token != null && token.isNotEmpty;
+    //   // bool loggingIn = state.uri.path == '/login';
+    //   // current path
+    //   final path = state.uri.path;
 
-    if (isFirstLaunch && path != '/welcome') {
-      // return '/login';
-      return '/welcome';
-    }
+    //   if (isFirstLaunch && path != '/welcome') {
+    //     // return '/login';
+    //     return '/welcome';
+    //   }
 
-    if (!loggedIn &&
-        path != '/login' &&
-        path != '/welcome' &&
-        path != '/signup') {
-      return '/login';
-    }
+    //   if (!loggedIn &&
+    //       path != '/login' &&
+    //       path != '/welcome' &&
+    //       path != '/signup') {
+    //     return '/login';
+    //   }
 
-    if (!isFirstLaunch && path == '/welcome') {
-      return '/';
-    }
+    //   if (!isFirstLaunch && path == '/welcome') {
+    //     return '/';
+    //   }
 
-    return null;
-  },
-  routes: <RouteBase>[
-    GoRoute(
-      name: 'home',
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) => const HomePage(),
-    ),
-    GoRoute(
-      name: 'login',
-      path: '/login',
-      builder: (context, state) => const LoginPage(),
-    ),
-    GoRoute(
-      name: 'signup',
-      path: '/signup',
-      builder: (context, state) => const SignupPage(),
-    ),
-    GoRoute(
-      name: 'app_colors',
-      path: '/colors',
-      builder: (context, state) => const ColorsPage(),
-    ),
-    GoRoute(
-      name: 'welcome',
-      path: '/welcome',
-      builder: (context, state) => const OnboardingScreen(),
-    ),
-    GoRoute(
-      name: 'explore',
-      path: '/explore',
-      builder: (context, state) => const ExplorePage(),
-    ),
-    GoRoute(
-      name: 'favorites',
-      path: '/favorites',
-      builder: (context, state) => const FavoritesPage(),
-    ),
-  ],
-);
+    //   return null;
+    // }, */
+    routes: <RouteBase>[
+      GoRoute(
+        name: 'home',
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const HomePage(),
+      ),
+      GoRoute(
+        name: 'login',
+        path: '/login',
+        builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        name: 'signup',
+        path: '/signup',
+        builder: (context, state) => const SignupPage(),
+      ),
+      GoRoute(
+        name: 'app_colors',
+        path: '/colors',
+        builder: (context, state) => const ColorsPage(),
+      ),
+      GoRoute(
+        name: 'welcome',
+        path: '/welcome',
+        builder: (context, state) => const OnboardingScreen(),
+      ),
+      GoRoute(
+        name: 'explore',
+        path: '/explore',
+        builder: (context, state) => const ExplorePage(),
+      ),
+      GoRoute(
+        name: 'favorites',
+        path: '/favorites',
+        builder: (context, state) => const FavoritesPage(),
+      ),
+    ],
+  );
+}
