98f8253b9d76020d4e18f4069d6c09c2682a3147
JoeWangu <joewangu2021@outlook.com>
2025-12-10 19:26:28 +0300

    feat: Refactor main function for improved app initialization and routing
    Updated the `main` function to include asynchronous initialization, integrating `FlutterNativeSplash` for a smoother launch experience. Implemented logic to determine the initial route based on user authentication status and onboarding completion, enhancing the app's navigation flow. The `InnApp` constructor was modified to accept a `GoRouter` instance for better routing management.

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,13 @@
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:inn/core/storage/secure_storage_provider.dart';
+import 'package:inn/core/utils/jwt_utils.dart';
 import 'package:inn/routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// A scroll behavior that enables mouse/track‑pad dragging for all axes.
 class DesktopDragScrollBehavior extends MaterialScrollBehavior {
@@ -32,12 +37,72 @@ class DesktopDragScrollBehavior extends MaterialScrollBehavior {
   };
 }
 
-void main() {
-  runApp(const ProviderScope(child: InnApp()));
+// void main() {
+//   runApp(const ProviderScope(child: InnApp()));
+// }
+Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  final container = ProviderContainer();
+  final storage = container.read(secureStorageProvider);
+  // final token = await storage.read(key: 'access_token');
+
+  final values = await Future.wait([
+    storage.read(key: 'access_token'),
+    storage.read(key: 'refresh_token'),
+    SharedPreferencesAsync().getBool('seen_onboarding'),
+  ]);
+
+  final accessToken = values[0] as String?;
+  final refreshToken = values[1] as String?;
+  final prefs = values[2] as bool?;
+
+  final bool seenOnboarding = prefs ?? false;
+
+  bool isLoggedIn = false;
+
+  if (accessToken != null && !JwtUtils.isExpired(accessToken)) {
+    // 1. Access Token is valid? Great.
+    isLoggedIn = true;
+  } else if (refreshToken != null && !JwtUtils.isExpired(refreshToken)) {
+    // 2. Access Token expired, BUT Refresh Token is still valid?
+    // We let them in! The 'AuthInterceptor' will detect the expired access token
+    // on the very first API call and silently refresh it in the background.
+    isLoggedIn = true;
+  } else {
+    // 3. Both are expired or missing? Login required.
+    isLoggedIn = false;
+  }
+
+  String initialPath;
+
+if (isLoggedIn) {
+    // IF LOGGED IN -> GO TO HOMEPAGE (Ignores onboarding status)
+    initialPath = '/';
+  } else if (!seenOnboarding) {
+    // IF NOT LOGGED IN + NOT ONBOARDED -> GO TO ONBOARDING
+    initialPath = '/welcome';
+  } else {
+    // IF NOT LOGGED IN + ALREADY ONBOARDED -> GO LOGIN
+    initialPath = '/login';
+  }
+
+  final appRouter = createRouter(initialPath);
+
+  runApp(
+    UncontrolledProviderScope(
+      container: container,
+      child: InnApp(router: appRouter,),
+    ),
+  );
+
+  FlutterNativeSplash.remove();
 }
 
 class InnApp extends StatelessWidget {
-  const InnApp({super.key});
+  final GoRouter router;
+  const InnApp({super.key, required this.router});
 
   @override
   Widget build(BuildContext context) {
