e715ef325d576c1182b1072942deb2d18adf4572
JoeWangu <joewangu2021@outlook.com>
2025-12-11 20:42:01 +0300

    feat: Add HouseCard widget for displaying house details
    Introduced a new `HouseCard` widget that presents house information, including an image, price, title, and location. This widget enhances the UI by providing a visually appealing representation of house listings, improving the overall user experience in the application.

---
+++ lib/presentation/shared/house_card.dart
@@ -0,0 +1,67 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:inn/data/models/house_model.dart';
+
+class HouseCard extends StatelessWidget {
+  final HouseModel house;
+  const HouseCard({required this.house});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Image
+          ClipRRect(
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+            child: CachedNetworkImage(
+              imageUrl: house.imageDetail.image,
+              height: 140,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => Container(color: Colors.grey[200]),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          ),
+          // Details
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "KES ${house.price.toStringAsFixed(0)}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(house.title, maxLines: 1, overflow: TextOverflow.ellipsis),
+                const SizedBox(height: 4),
+                Text(
+                  house.neighborhood?.name ??
+                      house.city?.name ??
+                      "Unknown Location",
+                  style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
