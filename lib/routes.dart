3931899db25153b0e18ca15dac033e067c2f12b4
JoeWangu <joewangu2021@outlook.com>
2025-12-12 18:38:19 +0300

    feat: Update routing to include new pages and restructure navigation
    Enhanced the routing configuration by adding new routes for house details and search pages, while restructuring the navigation to utilize a bottom navigation bar with a StatefulShellRoute. This update improves the user experience by providing a more organized navigation flow within the application.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:inn/data/models/house_model.dart';
 import 'package:inn/debug.dart';
 import 'package:inn/presentation/pages/auth/login.dart';
 import 'package:inn/presentation/pages/auth/signup.dart';
 import 'package:inn/presentation/pages/explore/explore.dart';
 import 'package:inn/presentation/pages/favorites/favorites.dart';
 import 'package:inn/presentation/pages/home/home.dart';
+import 'package:inn/presentation/pages/home/house_details_page.dart';
+import 'package:inn/presentation/pages/home/search_page.dart';
 import 'package:inn/presentation/pages/onboarding/onboarding.dart';
+import 'package:inn/presentation/pages/profile/profile_page.dart';
+import 'package:inn/presentation/shared/scaffold_with_nav_bar.dart';
 
 // final GoRouter router =
 GoRouter createRouter(String initialPath) {
   return GoRouter(
     initialLocation: initialPath,
-/*     // redirect: (context, state) async {
+    /*     // redirect: (context, state) async {
     //   final SharedPreferencesAsync prefs = SharedPreferencesAsync();
     //   final bool seenOnboarding =
     //       await prefs.getBool('seen_onboarding') ?? false;
@@ -44,12 +49,7 @@ GoRouter createRouter(String initialPath) {
     //   return null;
     // }, */
     routes: <RouteBase>[
-      GoRoute(
-        name: 'home',
-        path: '/',
-        builder: (BuildContext context, GoRouterState state) =>
-            const HomePage(),
-      ),
+      // Routes WITHOUT the Bottom Bar
       GoRoute(
         name: 'login',
         path: '/login',
@@ -70,15 +70,66 @@ GoRouter createRouter(String initialPath) {
         path: '/welcome',
         builder: (context, state) => const OnboardingScreen(),
       ),
-      GoRoute(
-        name: 'explore',
-        path: '/explore',
-        builder: (context, state) => const ExplorePage(),
+      // Routes WITH the Bottom Bar (Wrapped in Shell)
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return ScaffoldWithNavBar(navigationShell: navigationShell);
+        },
+        branches: [
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: 'home',
+                path: '/',
+                builder: (BuildContext context, GoRouterState state) =>
+                    const HomePage(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: 'explore',
+                path: '/explore',
+                builder: (context, state) => const ExplorePage(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: 'favorites',
+                path: '/favorites',
+                builder: (context, state) => const FavoritesPage(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: 'profile',
+                path: '/profile',
+                builder: (context, state) => const ProfilePage(),
+              ),
+            ],
+          ),
+        ],
       ),
       GoRoute(
-        name: 'favorites',
-        path: '/favorites',
-        builder: (context, state) => const FavoritesPage(),
+        name: 'house-details',
+        path: '/house-details',
+        builder: (context, state) {
+          final house = state.extra as Map<String, dynamic>;
+          return HouseDetailsPage(
+            house: house['house'] as HouseModel,
+            heroTag: house['tag'] as String,
+          );
+        },
+      ),
+      GoRoute(
+        name: 'search',
+        path: '/search',
+        builder: (context, state) => const SearchPage(),
       ),
     ],
   );
