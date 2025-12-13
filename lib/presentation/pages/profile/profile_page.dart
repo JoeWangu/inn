d73ed14832880615dd0868ba57587c1f5ce5063f
JoeWangu <joewangu2021@outlook.com>
2025-12-13 17:22:32 +0300

    feat: Refactor ProfilePage to use ConsumerWidget and enhance UI layout
    Updated ProfilePage to extend ConsumerWidget for improved state management. Enhanced the UI with a user avatar, name display, and a list of options including My Properties, Settings, Help & Support, and Log Out. This refactor improves user interaction and visual presentation on the profile page.

--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -1,21 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class ProfilePage extends StatefulWidget {
+class ProfilePage extends ConsumerWidget {
   const ProfilePage({super.key});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final ColorScheme cs = Theme.of(context).colorScheme;
 
     return Scaffold(
       backgroundColor: cs.onPrimary,
-      appBar: AppBar(title: Text('Profile Page')),
-      body: Column(children: [Placeholder()]),
+      appBar: AppBar(title: const Text('Profile')),
+      body: Column(
+        children: [
+          const SizedBox(height: 20),
+          const CircleAvatar(
+            radius: 40,
+            backgroundImage: AssetImage('assets/img_assets/avatar.png'),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Jay',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 30),
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.home_work),
+                  title: const Text('My Properties'),
+                  trailing: const Icon(Icons.chevron_right),
+                  onTap: () {
+                    context.pushNamed('my-properties');
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Settings'),
+                  trailing: const Icon(Icons.chevron_right),
+                  onTap: () {
+                    // context.pushNamed('settings');
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.help),
+                  title: const Text('Help & Support'),
+                  trailing: const Icon(Icons.chevron_right),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.logout, color: Colors.red),
+                  title: const Text(
+                    'Log Out',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  onTap: () {
+                    // Implement logout logic
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
