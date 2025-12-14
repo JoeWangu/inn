795b774c50b55da445ff28331a671534167dd4e0
JoeWangu <joewangu2021@outlook.com>
2025-12-14 20:04:10 +0300

    feat: Update FavoritesPage to display user favorites with improved UI
    Refactored FavoritesPage to utilize FavoritesController for managing user favorites. Implemented asynchronous loading of favorite houses, displaying a loading indicator, and handling errors. Enhanced the UI with a ListView to show favorite items using ItemCard, improving user experience when viewing favorites.

--- lib/presentation/pages/favorites/favorites.dart
+++ lib/presentation/pages/favorites/favorites.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:inn/presentation/controllers/favorites_controller/favorites_controller.dart';
+import 'package:inn/presentation/shared/item_card.dart';
+
 class FavoritesPage extends ConsumerStatefulWidget {
   const FavoritesPage({super.key});
 
@@ -9,16 +12,35 @@ class FavoritesPage extends ConsumerStatefulWidget {
 }
 
 class _FavoritesPageState extends ConsumerState<FavoritesPage> {
-  bool showCoupon = true;
-
   @override
   Widget build(BuildContext context) {
+    final favoritesAsync = ref.watch(favoriteHousesProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites'),
-        centerTitle: true,
-        // actions: [],
+      appBar: AppBar(title: const Text('Favorites'), centerTitle: true),
+      body: favoritesAsync.when(
+        data: (houses) {
+          if (houses.isEmpty) {
+            return const Center(child: Text("No favorites yet"));
+          }
+          return ListView.separated(
+            padding: const EdgeInsets.all(16),
+            itemCount: houses.length,
+            separatorBuilder: (context, index) => const SizedBox(height: 16),
+            itemBuilder: (context, index) {
+              final house = houses[index];
+              return ItemCard(
+                house: house,
+                tagPrefix: 'fav',
+                index: index,
+                cardHeight: 300,
+                cardWidth: double.infinity,
+              );
+            },
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (e, s) => Center(child: Text("Error loading favorites")),
       ),
     );
   }
