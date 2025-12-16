3c616de4717232a7fa9b4a631eecf40107f7d6ae
JoeWangu <joewangu2021@outlook.com>
2025-12-16 13:47:12 +0300

    feat: Integrate LocalAuth plugin for enhanced authentication support
    Added the LocalAuth plugin to both macOS and Windows platforms, enabling biometric authentication capabilities. This integration enhances the app's security features by allowing users to authenticate using their device's biometric sensors, improving user experience and security management.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import dynamic_color
 import file_selector_macos
 import flutter_secure_storage_macos
+import local_auth_darwin
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
